9e89b66d20d513fa2bf832136aa94022b061ce54
Pabatso Tejane <87855930+PabatsoTejane@users.noreply.github.com>
2023-03-05 21:46:42 +0200

    Test streamlit 2
    Checking if app works

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,24 @@
 
-# Streamlit dependencies
 import streamlit as st
 import joblib,os
-
-# Libraries to be used in data cleaning and model
 import string
-from sklearn import metrics
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import f1_score
-import re
-import warnings
-
-warnings.filterwarnings('ignore')
 
 # Data dependencies
 import pandas as pd
 import numpy as np
-
-#@st.cache(allow_output_mutation=True)
-#def get_data(filename):
-#	data = pd.read_csv(filename)
-
-#	return data
-
-# Load your raw data
-# raw_tweets = get_data("data//train.csv")
-# tweets = get_data("data//train.csv")
-# test_data = get_data("data//test_with_no_labels.csv")
 
 # The main function where we will build the actual app
 def main():
 	#Tweet Classifier App with Streamlit 
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.image('images//DataDot.PNG')
 	st.title("Climate Change Belief Classifier")
-	st.sidebar.image('images//DataDot.PNG')
 	st.sidebar.subheader('About Data Dot Inc.')
 	st.sidebar.markdown('Data Dot Inc. is a consultancy firm that uses data science processes to solve your everyday marketing problems')
 	st.sidebar.markdown('    ')
 	# Creating sidebar with selection box 
 	# you can create multiple pages this way
-	options = ["Project Summary", "Exploring The Data", "Prediction", " Additional Information"]
+	options = ["Project Summary", "Exploring The Data", " Additional Information"]
 	selection = st.sidebar.selectbox("App Navigation options", options)
 	st.sidebar.markdown('    ')
 	st.sidebar.subheader('Contact us')
@@ -54,8 +26,6 @@ def main():
 	st.sidebar.markdown(' * Tel: 011 442 0000')
 	st.sidebar.markdown(' * Twitter: @DataDot')
 	st.sidebar.markdown(' * LinkedIn: www.linkedin.com/in/DataDot')
-
-	
 
 	# Building the "Summary" page
 	if selection == "Project Summary":
@@ -74,13 +44,10 @@ def main():
 	if selection == "Exploring The Data":
 		st.subheader("Exploratory data analysis visuals")
 		st.markdown("The distribution of the four possible sentiments")
-		st.image('images//TwitterValue.PNG')
 		st.markdown("   ")
 		st.markdown("The Buzzwords used in the tweets.")
-		st.image('images//wordcloud.png')
 		st.markdown("   ")
 		st.markdown("The most popular hashtags")
-		st.image('images//frequent.png')
 
 	# Building the "Information" page
 	if selection == " Additional Information":
@@ -92,69 +59,7 @@ def main():
 		st.markdown("* **K-Neighnours Classifier**: KNN regression is a non-parametric method that, in an intuitive manner, approximates the association between independent variables and the continuous outcome by averaging the observations in the same neighbourhood. ")
 		st.markdown('For more indepth information on these models, vist: https://monkeylearn.com/blog/classification-algorithms/')
 
-		st.subheader("A look into the data")
-		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw_tweets[['sentiment', 'message']]) # will write the raw df to the page
-
-	if selection == 'Peril risk':
-		st.subheader('Peril risk in Hoedspruit')
-		data_source = ['Select option','Hail', 'Fire'] # differentiating between a single text and a dataset input
-		source_selection = st.selectbox('What to classify?', data_source)
-
         # Loading model pickle files
 	#	def load_prediction_models(model_file):
 	#		loaded_models = joblib.load(open(os.path.join(model_file),"rb"))
 	#		return loaded_models
-		
-# 		if source_selection == 'Data Frame':
-#             ### DATA FRAME TWEET CLASSIFICATION ###
-# 			st.subheader('DataFrame tweet classification')
-# 			text_input = st.file_uploader("Choose a CSV file", type="csv")
-# 			if text_input is not None:
-# 				text_input = pd.read_csv(text_input)
-				
-# 			uploaded_dataset = st.checkbox('See uploaded dataset')
-
-# 			if uploaded_dataset:
-# 				st.dataframe(text_input.drop('sentiment', axis=1).head(10))
-
-# 			st.markdown("   ")
-# 			st.image("images//Matrix.PNG")
-
-# 			ml_models = ["Linear SVC","Multinomial NB","K-Neighbours"]
-# 			model_choice = st.selectbox("Choose ML Model",ml_models)
-			
-
-# 			if st.button('Classify'):
-				
-# 				text_input['message'] = text_input['message'].apply(cleaner)
-# 				text_input = lemmatizer(tweets)
-
-# 				X = text_input['message']
-
-# 				if model_choice == 'Linear SVC':
-# 					predictor = joblib.load(open(os.path.join("models//LinearSVC.pkl"),"rb"))
-# 					prediction = predictor.predict(X)
-
-# 				if model_choice == 'Multinomial NB':
-# 					predictor = load_prediction_models("models//MultinomialNB.pkl")
-# 					prediction = predictor.predict(X)
-            
-# 				if model_choice == 'K-Neighbours':
-# 					predictor = load_prediction_models("models//KNeighbours.pkl")
-# 					prediction = predictor.predict(X)
-
-# 				prediction_dict = {-1: 'Anti', 0: 'Neutral',1: 'Pro',2: 'News'}
-# 				funt = lambda x: prediction_dict[x]
-# 				a = list(map(funt,prediction))
-# 				results = pd.DataFrame(a)
-		
-# 				results["tweet_id"] = text_input["tweetid"]
-# 				results.columns = ['Predicted_sentiment', 'Tweet_id'] #renaming result dataframe columns
-# 				results.set_index('Tweet_id', inplace=True)
-# 				st.success(st.dataframe(results))
-
-				
-if __name__ == '__main__':
-	main()
-
