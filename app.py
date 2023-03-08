e7869b0e9d97665e5afecfbd304cd461de451267
PabatsoTejane <tejanepabatso@gmail.com>
2023-03-08 13:30:57 +0200

    Page 2

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np 
 
-header  = st.container()
-dataset = st.container()
-feature = st.container()
-model = st.container()
+Page1  = st.container()
+Page2 = st.container()
+Page3 = st.container()
+Page4 = st.container()
 
-with header:
+with Page1:
     st.title('Anomaly detection')
     st.sidebar.image('lombard2.jpg',use_column_width=True)
     st.sidebar.markdown('    ')
@@ -23,6 +23,7 @@ with header:
     st.sidebar.markdown('    ')
     st.sidebar.subheader('Contact us')
     st.sidebar.markdown(' * Email: DataScience@lombardins.onmicrosoft.com')
+
     if selection == "Project Summary":
         #st.subheader('Project Problem Statement')
         st.write('''Climate change has resulted in the occurrence of unpredictable and severe weather events. The occurrence of these events may result in the loss of crops and vegetation, which can be catastrophic and detrimental to the livelihoods of the farmers affected. Crop insurance has become more popular with farmers being more aware of the benefits of indemnifying themselves against the loss of their crops from such events.  
@@ -40,4 +41,6 @@ with header:
         High relative humidity  
         High wind speeds  
         Low temperatures''')
+
+with Page2:
     if selection == "Exploring The Data":
