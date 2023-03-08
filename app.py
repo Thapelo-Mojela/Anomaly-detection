12b9de5bc68ac2da600a2d538095261c1424b059
PabatsoTejane <tejanepabatso@gmail.com>
2023-03-08 10:51:04 +0200

    changed logo

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = st.container()
 
 with header:
     st.title('Anomaly detection')
-    st.sidebar.image('lombard.png')
+    st.sidebar.image('lombard2.jpg')
     st.sidebar.markdown('    ')
     options = ["Project Summary", "Exploring The Data", "Peril Rating", " Additional Information"]
     selection = st.sidebar.selectbox(" **Page Navigation options** ", options)
