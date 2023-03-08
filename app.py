2f6c970aaf36a05327cc129bd4ff2e8fb266293b
PabatsoTejane <tejanepabatso@gmail.com>
2023-03-08 13:51:49 +0200

    moving heading to page 1

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ Page3 = st.container()
 Page4 = st.container()
 
 with Page1:
-    st.title('Anomaly detection')
     st.sidebar.image('lombard2.jpg',use_column_width=True)
     st.sidebar.markdown('    ')
     options = ["Project Summary", "Exploring The Data", "Peril Rating"]
@@ -25,16 +24,16 @@ with Page1:
     st.sidebar.markdown(' * Email: DataScience@lombardins.onmicrosoft.com')
 
     if selection == "Project Summary":
-        #st.subheader('Project Problem Statement')
+        st.title('Anomaly detection')
         st.write('''Climate change has resulted in the occurrence of unpredictable and severe weather events. The occurrence of these events may result in the loss of crops and vegetation, which can be catastrophic and detrimental to the livelihoods of the farmers affected. Crop insurance has become more popular with farmers being more aware of the benefits of indemnifying themselves against the loss of their crops from such events.  
         The purpose of this project is to explore the occurrence of these perils and provide guidance on pricing, in order to offer a reasonable product to the farmer while preventing loss on the insurers side.''')
         st.markdown('    ')
         st.markdown('The main perils we are interested in for crop insurance are hail are fire. Based on literature these are the factors/weather conditions that influence the perils:')
         st.markdown('* **Fire conditions**')
         st.write('''In the presence of dry vegetation:  
-        Lightning(ignition source), Low relative humidity, High wind speeds, High temperatures''')
+        Lightning(ignition source), low relative humidity, high wind speeds and high temperatures''')
         st.markdown('* **Hail conditions**')
-        st.write('''Heavy rainfall, High relative humidity, High wind speeds, Low temperatures''')
+        st.write('''Heavy rainfall, high relative humidity, high wind speeds and low temperatures''')
 
     elif selection == "Exploring The Data":
         st.write('prediction')
