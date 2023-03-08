63c0f0a6f51ec4c0c9d62fb0af4f6271fb0fcfe5
PabatsoTejane <tejanepabatso@gmail.com>
2023-03-08 11:36:28 +0200

    line edit

--- app.py
+++ app.py
@@ -24,8 +24,9 @@ with header:
     st.sidebar.subheader('Contact us')
     st.sidebar.markdown(' * Email: DataScience@lombardins.onmicrosoft.com')
     if selection == "Project Summary":
-        st.subheader('Project Problem Statement')
+        #st.subheader('Project Problem Statement')
         st.markdown('Climate change has resulted in the occurrence of unpredictable and severe weather events. The occurrence of these events may result in the loss of crops and vegetation, which can be catastrophic and detrimental to the livelihoods of the farmers affected. Crop insurance has become more popular with farmers being more aware of the benefits of indemnifying themselves against the loss of their crops from such events.')
         st.markdown('    ')       
         st.markdown('The purpose of this project is to explore the occurrence of these perils and provide guidance on pricing, in order to offer a reasonable product to the farmer while preventing loss on the insurers side.')
-
+        st.markdown('    ')
+        st.markdown('The main perils we are interested in for crop insurance are hail are fire. Based on literature these are the factors/weather conditions that influence the perils:')
