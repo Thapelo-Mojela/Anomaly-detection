308993cac9ee56c27c35cbb35d10f81ee2998632
PabatsoTejane <tejanepabatso@gmail.com>
2023-03-09 10:59:57 +0200

    retry

--- app.py
+++ app.py
@@ -61,11 +61,11 @@ with Page1:
             fire_severity_score = 0.76
             fire_frequecy_score = 0.12
             if choice == 'Hail risk':
-                Hail_risk = round(hail_severity_score*hail_ferquency_score*100,2)
+                round(hail_severity_score*hail_ferquency_score*100,2)
                 #st.write('The risk of hail in Hoedspruit based on the weather data analysed is:', Hail_risk ,'%')
 
             elif choice == 'Fire risk':
-                Fire_risk = round(fire_severity_score*fire_frequecy_score*100,2)
+                round(fire_severity_score*fire_frequecy_score*100,2)
                 #st.write('The risk of fire in Hoedspruit based on the weather data analysed is:', Fire_risk ,'%')
         if st.button("Calculate risk rating"):
             Rrating()
